bb5c5eaf663c4bd86ebf557ecc0859c38b40fd48
Sean Conaty <anxiolydiot@Seans-MacBook-Pro.local>
2016-01-17 20:10:23 -0500

    started with a switch case to call functions with a given parameter but seems messy, making a new brnch to try a tidier alternative

--- liri.js
+++ liri.js
@@ -1,12 +1,37 @@
+
+
 var fs = require('fs');
 var myTwitterKeys = require('./keys.js');
-var params= process.argv.slice(2);
+//takes of 'node' and .js file from aruments in command line, leaves arument(s) of interest for executing a given feature in the app
+var parameters= process.argv.slice(2);
 
 
-fs.readFile(params[0], 'utf8', function (err, data){
+fs.readFile(parameters[0], 'utf8', function (err, data){
   var output = data.split(',');
   for(var i = 0; i < output.length;i++){
     console.log(output[i])
   }
 })
 
+//switch case which calls the function associated with a given arument specifed by the user
+function excecuteApp (parameters){
+  switch (parameters [0] {
+    case "tweets":
+      tweets();
+      break;
+    case "spotify":
+      spotify();
+      break;
+    case "movie":
+      movie();
+      break;
+    case "help":
+      manual();
+      break
+    case "":
+      noArgument();
+      break;
+    case "":
+      notValid();
+      break;
+    case 
