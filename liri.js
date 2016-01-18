71ecdd1cc0fa68c9592e03e73b7b583c871540fb
Sean Conaty <anxiolydiot@Seans-MacBook-Pro.local>
2016-01-17 20:45:42 -0500

    going in another idrection away from the swtich case. Trying more of a lookup method, have to write out the actual functions but structure more or less set up

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var myTwitterKeys = require('./keys.js');
-var params= process.argv.slice(2);
+var parameters= process.argv.slice(2);
 
 
 fs.readFile(params[0], 'utf8', function (err, data){
@@ -10,3 +10,53 @@ fs.readFile(params[0], 'utf8', function (err, data){
   }
 })
 
+// function excecuteApp (parameters){
+//   switch (parameters [0] {
+//     case "tweets":
+//       tweets();
+//       break;
+//     case "spotify":
+//       spotify();
+//       break;
+//     case "movie":
+//       movie();
+//       break;
+//     case "help":
+//       manual();
+//       break
+//     case "":
+//       noArgument();
+//       break;
+//     case "":
+//       notValid();
+//       break;
+//     case 
+
+function excecuteApp (parameters){
+  var parameter;
+  var allParameters = {
+    'tweets': function () {
+      get the tweets func;
+
+    },
+    'spotify': function () {
+      get the spotify func;
+
+    },
+    'movie': function () {
+      get the movie func;
+
+    },
+     'help': function () {
+      con log manual from text file;
+
+    },
+     'default': function () {
+    console.log( Please Type a Valid Parameter or Type <help> To Read The Manual);
+    }
+  };
+
+  (allParameters[parameters] || allParameters['default'])();
+
+  }
+}
